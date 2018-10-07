b4050186242e95f5e21ad34f2c3dff1a0541f141
rparamo <rparamo11@gmail.com>
2018-10-07 03:35:39 -0500

    added connection for heroku

--- server.js
+++ server.js
@@ -22,7 +22,15 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost/mongoscraper_db');
+// mongoose.connect('mongodb://localhost/mongoscraper_db');
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper_db";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
